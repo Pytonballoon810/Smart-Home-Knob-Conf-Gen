6c894df5fde1d53111c40c8395e6d33439e83111
Philipp Hofmann <philipp@hofmann-ebs.de>
2025-03-10 04:32:08 +0100

    Refactor imports in App.tsx to streamline code

--- rusty-smart-knob-conf-gen/src/App.tsx
+++ rusty-smart-knob-conf-gen/src/App.tsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import ConfigItem, { 
-  interfaceTypes, 
-  detentOptions, 
+import ConfigItem, {
   requiresMinMax, 
   canHaveStep 
 } from "./components/ConfigItem";
